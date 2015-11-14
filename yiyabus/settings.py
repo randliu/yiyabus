db7c2d4fbecd8eb2ce8423f5d3e7b1a13218871b
randliu <buckler.liu@gmail.com>
2015-11-14 23:43:54 +0800

    yaya

--- yiyabus/settings.py
+++ yiyabus/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -100,3 +102,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT =  '/Users/rand/Documents/git/yiyabus/pkgs'
+MEDIA_URL = '/site_media/' 
+
--- yiyabus/urls.py
+++ yiyabus/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/$', 'blog.views.index'),
+    url(r'^register/$', 'blog.views.register'),
+    url(r'^list/$', 'blog.views.list'),    
 ]
